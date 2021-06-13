e36de9d30c8d697b4127a9ac034585da24e2d9ed
labadcloyd <abadcloyd@gmail.com>
2021-06-13 01:10:19 +0000

    updated serverjs file

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const next = require("next");
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+const port = process.env.PORT || 3000
+require('dotenv').config({path:'./config.env'})
+const connectDb = require('./utilsServer/connectDB')
+connectDb();
+
+io.on("connection", (socket) => {
+  console.log('connected')
+  socket.on('join-room', (room)=>{
+    console.log('joined room')
+    socket.join(room)
+  })
+  socket.on('send-message', (messageForm, room)=>{
+    console.log(messageForm, room)
+    if(room){
+      io.in(room).emit('receive-message', messageForm)
+    }
+  })
+})
+
+nextApp.prepare().then(() => {
+  
+  app.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
