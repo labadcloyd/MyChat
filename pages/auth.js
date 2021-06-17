7814909dccf8643c7ff241b259d0066354e9e52a
labadcloyd <abadcloyd@gmail.com>
2021-06-17 20:10:19 +0000

    switched to client side rendering on queried chat page

--- pages/auth.js
+++ pages/auth.js
@@ -7,20 +7,17 @@ import { useEffect, useState } from 'react';
 function AuthPage() {
   const router = useRouter()
   const [ session, loading ] = useSession()
+
   if(loading) {
     return <>
       <h1>Loading...</h1>
     </>
   }
   if(!loading){
-    /* 
-    ! somehow router.push won't work without returning a component first 
-    * since in this case routerpush happens before any component mounts
-    */
     if(session){
       router.push('/')
       return <>
-        <h1>Loading...</h1>
+      <h1>Loading but should be redirecting</h1>
       </>
     }
     if(!session){
--- pages/chat/[queriedUsername].js
+++ pages/chat/[queriedUsername].js
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import Sidebar from '../../components/sidebar/sidebar'
 import Chat from '../../components/chat/chat'
 import {getSession, useSession} from 'next-auth/client'
-import {User} from '../../models/usermodel'
 import { useEffect, useState, useContext } from 'react'
 import { useRouter } from 'next/router'
 import axios from 'axios'
@@ -12,19 +11,18 @@ import {SocketContext} from '../../context/socketContext';
 export default function Home(props) {
 	const router = useRouter()
 	const socketio = useContext(SocketContext);
+	const [ session, loading ] = useSession()
 	
 	const selectedUser = router.query.queriedUsername
 	const [username, setUsername] = useState()
-	const [doesChatExist, setChatExists] =useState(false)
 	const [userChats, setUserChats] = useState()
 	const [chatID, setChatID] = useState(null)
 	const [currentChat, setCurrentChat] = useState(null)
-	const [isExistingChat, setIsExistingChat] = useState(doesChatExist)
-
+	const [isExistingChat, setIsExistingChat] = useState(false)
 
 	/* getting current chat */
 	async function getCurrentChat(){
-		const response = await axios.get('/api/getChat', {params:{queriedUsername:selectedUser, username:username}})
+		const response = await axios.get('/api/getChat', {params:{queriedUsername:selectedUser, username:session.user.name}})
 		const {data} = response
 		console.log(data)
 		setCurrentChat(data.messages)
@@ -32,10 +30,13 @@ export default function Home(props) {
 		socketio.emit('join-room', data.chatID)
 	}
 	useEffect(async()=>{
-		const [ session, loading ] = await useSession()
-		setUsername(session.user.name)
-		await getCurrentChat()
-	}, [router])
+		if(!loading && session){
+			setUsername(session.user.name)
+			await getCurrentChat()
+		}if(!loading && !session){
+			router.push('/auth')
+		}
+	}, [loading])
 
 	/* selecting chat */
 	async function handleSelectChat(selectedUsername){
@@ -46,24 +47,40 @@ export default function Home(props) {
 	/* turning off listener in order to for socketio to only listen once */
 	socketio.off('receive-message').on('receive-message', async(messageForm, room)=>{
 		if(!isExistingChat){
-			console.log(messageForm)
 			setCurrentChat([messageForm]);
 			setIsExistingChat(true);
-			console.log(messageForm)
 		}else if(isExistingChat){
 			setCurrentChat((prevValue)=>{return  [...prevValue, messageForm] } );
-			console.log(currentChat)
 		}
 	})
-	return (
-		<div className="chat-wrapper">
-			<Head>
-				<title>Chat App</title>
-				<meta name="description" content="" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<Sidebar username={username} handleSelectChat={handleSelectChat} userChats={userChats}/>
-			<Chat chatID={chatID} isExistingChat={isExistingChat} selectedUser={selectedUser} currentChat={currentChat} username={username} socket={socketio}/>
-		</div>
-	)
+
+	if(loading || !username){
+		return(
+			<div>
+				<h1>Loading...</h1>
+				<h1>Loading...</h1>
+				<h1>Loading...</h1>
+			</div>
+		)
+	}
+	if(!loading || username){
+		return (
+			<div className="chat-wrapper">
+				<Head>
+					<title>Chat App</title>
+					<meta name="description" content="" />
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
+				<Sidebar username={username} handleSelectChat={handleSelectChat} userChats={userChats}/>
+				{!currentChat && 
+					<div>
+						<h1>Loading...</h1>
+					</div>
+				}
+				{currentChat &&
+					<Chat chatID={chatID} isExistingChat={isExistingChat} selectedUser={selectedUser} currentChat={currentChat} username={username} socket={socketio}/>
+				}
+			</div>
+		)
+	}
 }
