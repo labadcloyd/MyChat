f95eb38b87c59d715fd4ba494d0c63e21eb5afa6
labadcloyd <abadcloyd@gmail.com>
2021-06-17 20:10:19 +0000

    switched homepage to client side render

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,59 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Sidebar from '../components/sidebar/sidebar'
-import Chat from '../components/chat/chat'
-import {getSession, useSession} from 'next-auth/client'
-import {User} from '../models/usermodel'
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import {SocketContext} from '../context/socketContext';
+import {getSession, useSession} from 'next-auth/client'
 
 export default function Home(props) {
   const router = useRouter()
-  const {session, userChats, username} = props
-	const [currentChat, setCurrentChat] = useState(null)
-	const [selectedUser, setSelectedUser] = useState()
-  const [isExistingChat, setIsExistingChat] = useState(false)
-  
   const socket = useContext(SocketContext);
+	const [ session, loading ] = useSession()
+  
+	const [username, setUsername] = useState()
+	const [userChats, setUserChats] = useState()
+	const [chatID, setChatID] = useState(null)
+	const [currentChat, setCurrentChat] = useState(null)
+	const [isExistingChat, setIsExistingChat] = useState(false)
+  
+  useEffect(async()=>{
+		if(!loading && session){
+			setUsername(session.user.name)
+		}if(!loading && !session){
+			router.push('/auth')
+		}
+	}, [loading])
+
 	socket.on('connect', (socket)=>{
 		console.log('connected')
 	})
+  
 	/* selecting chat handler */
 	async function handleSelectChat(selectedUsername){
     router.push(`/chat/${selectedUsername}`)
 	}
-  return (
-    <div className="chat-wrapper">
-      <Head>
-        <title>Chat App</title>
-        <meta name="description" content="" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Sidebar username={username} handleSelectChat={handleSelectChat} userChats={userChats}/>
-      <div>
-        Search for a user to start chatting
-      </div>
-    </div>
-  )
-}
-
-export async function getServerSideProps(context){
-  /* basic authentication */
-	const session = await getSession({req:context.req})
-	if(!session){
-		return{
-			redirect:{
-				destination: '/auth'
-			}
-		}
+  if(loading || !username){
+		return(
+			<div>
+				<h1>Loading...</h1>
+				<h1>Loading...</h1>
+				<h1>Loading...</h1>
+			</div>
+		)
 	}
-  /* finding the user */
-  const username = session.user.name
-  const user = await User.findOne({username:username})
-  if(!user){
-    return {
-      notFound: true
-    }
-  }
-  /* finding the user's chat */
-  const userChats = user.chats
-  return{
-    props:{
-      session:session,
-      userChats:userChats,
-      username:username
-    }
+  if(!loading ){
+    return (
+      <div className="chat-wrapper">
+        <Head>
+          <title>Chat App</title>
+          <meta name="description" content="" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Sidebar username={username} handleSelectChat={handleSelectChat} userChats={userChats}/>
+        <div>
+          Search for a user to start chatting
+        </div>
+      </div>
+    )
   }
 }
