b8dfc08ac39e24cc91c205960d459c77a2218636
labadcloyd <abadcloyd@gmail.com>
2021-06-26 20:10:19 +0000

    fixed getmorechat api from returning duplicate messages

--- pages/api/getMoreChat.js
+++ pages/api/getMoreChat.js
@@ -4,6 +4,18 @@ export default async function handler(req, res){
 	if(req.method === 'GET'){
 		const {currentChatLength, chatID } = req.query
 
+		/* 
+		!	I'M NOT SURE IF CONSTANTLY QUERYING THE CHAT DOCUMENT WOULD BE EFFICIENT
+		*/
+		const data = await Chats.find({chatID:chatID})
+		const chats = data[0].messages
+		if(chats.length === currentChatLength){
+			return res.status(201).json({hasReachedTop:true})
+		}
+		/* Using negative numbers to get the elements starting from the bottom */
+		const slicedChats = chats.slice(((currentChatLength * -1)-20), (currentChatLength * -1))
+		return res.status(201).json(slicedChats)
+
 		/* OLD QUERY */
 		// /*
 		// 	! There is a bug when using $slice and entering numbers for amount of objects to skip and amount of object to return.
